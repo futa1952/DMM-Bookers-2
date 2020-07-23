fca64d7d70cc80c15979c215a2c253a6186ce1d6
futa1952 <futa1952@icloud.com>
2020-07-23 10:44:31 +0900

    [delete] migrateFile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_043319) do
+ActiveRecord::Schema.define(version: 2020_07_18_023808) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_07_21_043319) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "name"
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
